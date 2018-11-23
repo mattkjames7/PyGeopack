174b9652b044abf02ba8d190887869d2bb943eb2
mattkjames7 <mattkjames7@gmail.com>
2018-11-23 21:28:08 +0000

    changed instalation

--- setup.py
+++ setup.py
@@ -1,30 +1,37 @@
 import setuptools
 from setuptools.command.install import install
 import os
+import atexit
+
+def PostInstall():
+	#store the current working directory
+	CWD = os.getcwd()
+		
+	#move into the libgeopack folder to make the module
+	print('PyGeopack: Running makefile')
+	os.chdir('PyGeopack/__data/libgeopack')
+	os.system('make')
+		
+	#move into the datafolder to extract the giant data file
+	print('PyGeopack: Extracting model data')
+	os.chdir('data')
+	os.system('7z x -y TSdata.bin.tar.7z')
+	os.system('tar -xf TSdata.bin.tar')
+		
+	#revert to original working directory
+	print('PyGeopack: reverting to original working directory')
+	os.chdir(CWD)		
+	
+
 
 class CustomInstallCommand(install):
 	"""Customized setuptools install command will hopefully decompress the data file and make the C++/Fortran code"""
-	def run(self):
+	def run(self, *args, **kwargs):
 		#go ahead with install
-		install.run(self)
-		
-		#store the current working directory
-		CWD = os.getcwd()
-		
-		#move into the libgeopack folder to make the module
-		print('PyGeopack: Running makefile')
-		os.chdir('PyGeopack/__data/libgeopack')
-		os.system('make')
-		
-		#move into the datafolder to extract the giant data file
-		print('PyGeopack: Extracting model data')
-		os.chdir('data')
-		os.system('7z x -y TSdata.bin.tar.7z')
-		os.system('tar -xf TSdata.bin.tar')
-		
-		#revert to original working directory
-		print('PyGeopack: reverting to original working directory')
-		os.chdir(CWD)		
+		#install.run(self)
+		super(new_install, self).__init__(*args, **kwargs)
+
+		atexit.register(PostInstall)
 
 
 
