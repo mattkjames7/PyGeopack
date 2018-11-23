a9e9df65470bd7e6f596ce63e38a46f20c2dc340
mattkjames7 <mattkjames7@gmail.com>
2018-11-23 17:32:08 +0000

    changed instalation

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PyGeopack",
-    version="0.0.1",
+    version="0.0.2",
     author="Matthew Knight James",
     author_email="mattkjames7@gmail.com",
     description="Geopack08 wrapper for Python",
