0bbc78c920ab535bfaa496ba6e321dd6166acea1
mattkjames7 <mattkjames7@gmail.com>
2018-11-26 11:52:43 +0000

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,84 +1,13 @@
 import setuptools
 from setuptools.command.install import install
 import os
-<<<<<<< HEAD
-# import atexit
-
-# def PostInstall():
-	# #store the current working directory
-	# CWD = os.getcwd()
-		
-	# #move into the libgeopack folder to make the module
-	# print('PyGeopack: Running makefile')
-	# os.chdir('PyGeopack/__data/libgeopack')
-	# os.system('make')
-		
-	# #move into the datafolder to extract the giant data file
-	# print('PyGeopack: Extracting model data')
-	# os.chdir('data')
-	# os.system('7z x -y TSdata.bin.tar.7z')
-	# os.system('tar -xf TSdata.bin.tar')
-		
-	# #revert to original working directory
-	# print('PyGeopack: reverting to original working directory')
-	# os.chdir(CWD)		
-	
-
-
-# class CustomInstallCommand(install):
-	# """Customized setuptools install command will hopefully decompress the data file and make the C++/Fortran code"""
-	# def __init__(self, *args, **kwargs):
-		# #go ahead with install
-		# #install.run(self)
-		# super(CustomInstallCommand, self).__init__(*args, **kwargs)
-
-# #		atexit.register(PostInstall)
-=======
-import atexit
-
-def PostInstall():
-	#store the current working directory
-	CWD = os.getcwd()
-		
-	#move into the libgeopack folder to make the module
-	print('PyGeopack: Running makefile')
-	os.chdir('PyGeopack/__data/libgeopack')
-	os.system('make')
-		
-	#move into the datafolder to extract the giant data file
-	print('PyGeopack: Extracting model data')
-	os.chdir('data')
-	os.system('7z x -y TSdata.bin.tar.7z')
-	os.system('tar -xf TSdata.bin.tar')
-		
-	#revert to original working directory
-	print('PyGeopack: reverting to original working directory')
-	os.chdir(CWD)		
-	
-
-
-class CustomInstallCommand(install):
-	"""Customized setuptools install command will hopefully decompress the data file and make the C++/Fortran code"""
-	def run(self, *args, **kwargs):
-		#go ahead with install
-		#install.run(self)
-		super(new_install, self).__init__(*args, **kwargs)
-
-		atexit.register(PostInstall)
->>>>>>> 85c321a3369f989a84f0ba181b6b994e9560b0c4
-
-
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="PyGeopack",
-<<<<<<< HEAD
-    version="0.0.4",
-=======
-    version="0.0.2",
->>>>>>> 85c321a3369f989a84f0ba181b6b994e9560b0c4
+    version="0.0.5",
     author="Matthew Knight James",
     author_email="mattkjames7@gmail.com",
     description="Geopack08 wrapper for Python",
@@ -88,18 +17,13 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: POSIX",
     ],
     install_requires=[
 		'numpy',
 	],
 	include_package_data=True,
-<<<<<<< HEAD
-	#cmdclass = { 'install':CustomInstallCommand },
-=======
-	cmdclass = { 'install':CustomInstallCommand },
->>>>>>> 85c321a3369f989a84f0ba181b6b994e9560b0c4
 )
 
 
