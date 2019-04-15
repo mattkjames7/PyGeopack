fa6d0ac5dd4c570a0bdd1ddd9b8cad70e3acbf93
Matt <mattkjames7@gmail.com>
2019-04-15 11:28:11 +0100

    updated version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PyGeopack",
-    version="0.0.9",
+    version="0.0.10",
     author="Matthew Knight James",
     author_email="mattkjames7@gmail.com",
     description="Geopack08 wrapper for Python",
