720e3ccfcd4e17b3ad682da876017747f919714e
mattkjames7 <mattkjames7@gmail.com>
2020-02-20 12:06:07 +0000

    Fixed MltE

--- PyGeopack/__data/libgeopackdp/TraceRoutines.c
+++ PyGeopack/__data/libgeopackdp/TraceRoutines.c
@@ -94,25 +94,21 @@ void GetMagEquatorFP(double *x, double *y, double *z, double *s, double *R, int 
 	double Rmx, rho, xt, yt, zt, a;
 	
 
-	//for (i=0;i<n;i++) {
-		//printf("a %f %f %f %f %f\n", s[i],x[i],y[i],z[i],R[i]);
-	//}
+
 
 	/*find the maximum R*/
 	argmax(R,n,&Rmx,&Imx);
 
-	//for (i=0;i<n;i++) {
-		//printf("b %f %f %f %f %f\n", s[i],x[i],y[i],z[i],R[i]);
-	//}
-
+	/*Convert to SM */
+	smgsw_08_(&xt,&yt,&zt,&x[Imx],&y[Imx],&z[Imx],&dirn);
 
 	if (x[Imx] < 0.0) {
 		/* if we are on the night side, just use the furthest point */
 		Lshell[0] = R[Imx];
-		MltE[0] = fmod(atan2(-y[Imx],-x[Imx])*12.0/M_PI + 24.0,24.0);
+		MltE[0] = fmod(atan2(-yt,-xt)*12.0/M_PI + 24.0,24.0);
 	} else {
 		/*convert to SM and check that it is within 10 degrees of SM x-y plane*/
-		smgsw_08_(&xt,&yt,&zt,&x[Imx],&y[Imx],&z[Imx],&dirn);
+		
 		rho = sqrt(xt*xt + yt*yt);
 		a = acos(rho/Rmx)*180.0/M_PI;
 		if (a > 10.0) {
@@ -124,7 +120,7 @@ void GetMagEquatorFP(double *x, double *y, double *z, double *s, double *R, int 
 		} else { 
 			/*just use the largest R*/
 			Lshell[0] = R[Imx];
-			MltE[0] = fmod(atan2(-y[Imx],-x[Imx])*12.0/M_PI + 24.0,24.0);	
+			MltE[0] = fmod(atan2(-yt,-xt)*12.0/M_PI + 24.0,24.0);	
 		}
 	}
 	
