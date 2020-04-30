8fcce9ba4e28260bf77544d0f5e74646ae2d6219
mattkjames7 <mattkjames7@gmail.com>
2020-04-30 08:56:31 +0100

    removed pause from T01 model, updated version, new build

--- build/lib/PyGeopack/__data/libgeopackdp/T01_01.f
+++ build/lib/PyGeopack/__data/libgeopackdp/T01_01.f
@@ -99,7 +99,7 @@ C
       PRINT *,
      * '  ATTENTION:  THE MODEL IS VALID SUNWARD FROM X=-15 Re ONLY,'
       PRINT *,'              WHILE YOU ARE TRYING TO USE IT AT X=', X
-      PAUSE
+
       ENDIF
 C
       PDYN=PARMOD(1)
