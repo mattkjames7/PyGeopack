1acaa8fce74075bfc4a1e6ee8c6b1659d556efb8
mattkjames7 <mattkjames7@gmail.com>
2021-04-13 09:14:20 +0100

    updated version

--- setup.py
+++ setup.py
@@ -5,9 +5,34 @@ import os
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+def getversion():
+	'''
+	read the version string from __init__
+	
+	'''
+	#get the init file path
+	thispath = os.path.abspath(os.path.dirname(__file__))+'/'
+	initfile = thispath + 'Arase/__init__.py'
+	
+	#read the file in
+	f = open(initfile,'r')
+	lines = f.readlines()
+	f.close()
+	
+	#search for the version
+	version = 'unknown'
+	for l in lines:
+		if '__version__' in l:
+			s = l.split('=')
+			version = s[-1].strip().strip('"').strip("'")
+			break
+	return version
+	
+version = getversion()
+
 setuptools.setup(
     name="PyGeopack",
-    version="0.2.8",
+    version=version,
     author="Matthew Knight James",
     author_email="mattkjames7@gmail.com",
     description="Geopack08 wrapper for Python",
