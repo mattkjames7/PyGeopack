5d25661304ba8e0ac634470c30477a3a7c5356c6
mattkjames7 <mattkjames7@gmail.com>
2021-07-14 14:24:40 +0100

    added GEI/GEO conversion code

--- PyGeopack/__data/libgeopackdp/ConvCoords.h
+++ PyGeopack/__data/libgeopackdp/ConvCoords.h
@@ -146,3 +146,97 @@ void MAGtoGEOUT_LL(	double *MLon, double *MLat, int n,
 					double *Vx, double *Vy, double *Vz, 
 					int *Date,float *ut, 
 					double *Lon, double *Lat);
+
+
+
+
+
+
+/***********************************************************************
+ * NAME : 			void GEItoGEO(	Xin, Yin, Zin, Vx, Vy, Vz, recalc,
+ *									Year, DayNo, Hr, Mn, Sc, 
+ *									*Xout, *Yout, *Zout)
+ * 
+ * DESCRIPTION : 	Wrapper for converting GEI to GEO coordinates
+ * 
+ * INPUTS : 
+ * 		double	Xin		x GEI coordinate (R_E)
+ * 		double	Yin		y GEI coordinate (R_E)
+ * 		double	Zin		z GEI coordinate (R_E)
+ * 		double 	Vx		x-component of SW velocity in km/s
+ * 		double 	Vy		y-component of SW velocity in km/s
+ * 		double 	Vz		z-component of SW velocity in km/s
+ * 		int		recalc	Set to one ro call recalc_08_
+ * 		int		Year	Year
+ * 		int		DayNo	Day number for that year
+ * 		int		Hr		Hours
+ * 		int		Mn		Minutes
+ * 		int		Sc		Seconds
+ * 
+ * OUTPUTS : 
+ * 		double	*Xout	x GEO coordinate (R_E)
+ * 		double	*Yout	y GEO coordinate (R_E)
+ * 		double	*Zout	z GEO coordinate (R_E)
+ * 
+ * RETURNS : 
+ * 		void
+ * 
+ * PROCESS : 
+ * 		[1] Calls recalc_08_ function in FORTRAN (if recalc != 0)
+ * 		[2] Calls FORTRAN code to convert from GEI to GEO coordinates
+ * 
+ * ********************************************************************/
+void GEItoGEO(	double Xin, double Yin, double Zin, 
+				double Vx, double Vy, double Vz, int recalc,
+				int Year, int DayNo, int Hr, int Mn, int Sc, 
+				double *Xout, double *Yout, double *Zout);
+				
+void GEItoGEOUT(	double *Xin, double *Yin, double *Zin, int n, 
+					double *Vx, double *Vy, double *Vz, 
+					int *Date, float *ut, 
+					double *Xout, double *Yout, double *Zout);
+
+
+/***********************************************************************
+ * NAME : 			void GEOtoGEI(	Xin, Yin, Zin, Vx, Vy, Vz, recalc,
+ *									Year, DayNo, Hr, Mn, Sc, 
+ *									*Xout, *Yout, *Zout)
+ * 
+ * DESCRIPTION : 	Wrapper for converting GEO to GEI coordinates
+ * 
+ * INPUTS : 
+ * 		double	Xin		x GEO coordinate (R_E)
+ * 		double	Yin		y GEO coordinate (R_E)
+ * 		double	Zin		z GEO coordinate (R_E)
+ * 		double 	Vx		x-component of SW velocity in km/s
+ * 		double 	Vy		y-component of SW velocity in km/s
+ * 		double 	Vz		z-component of SW velocity in km/s
+ * 		int		recalc	Set to one ro call recalc_08_
+ * 		int		Year	Year
+ * 		int		DayNo	Day number for that year
+ * 		int		Hr		Hours
+ * 		int		Mn		Minutes
+ * 		int		Sc		Seconds
+ * 
+ * OUTPUTS : 
+ * 		double	*Xout	x GEI coordinate (R_E)
+ * 		double	*Yout	y GEI coordinate (R_E)
+ * 		double	*Zout	z GEI coordinate (R_E)
+ * 
+ * RETURNS : 
+ * 		void
+ * 
+ * PROCESS : 
+ * 		[1] Calls recalc_08_ function in FORTRAN (if recalc != 0)
+ * 		[2] Calls FORTRAN code to convert from GEO to GEI coordinates
+ * 
+ * ********************************************************************/
+void GEOtoGEI(	double Xin, double Yin, double Zin, 
+				double Vx, double Vy, double Vz, int recalc,
+				int Year, int DayNo, int Hr, int Mn, int Sc, 
+				double *Xout, double *Yout, double *Zout);
+
+void GEOtoGEIUT(	double *Xin, double *Yin, double *Zin, int n, 
+					double *Vx, double *Vy, double *Vz, 
+					int *Date, float *ut, 
+					double *Xout, double *Yout, double *Zout);
