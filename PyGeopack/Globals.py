dcecdb7646c26ae9c9d1202a37983d47c790c4b0
matt <mattkjames7@gmail.com>
2024-02-21 00:34:14 +0000

    rename variable

--- PyGeopack/Globals.py
+++ PyGeopack/Globals.py
@@ -2,7 +2,7 @@ import os
 ModulePath = os.path.dirname(__file__)+'/'
 
 def checkWarning():
-    warnVar = os.getenv("PYGEOPACK_NOWARN","")
+    warnVar = os.getenv("GEOPACK_NOWARN","")
     if warnVar.lower() in ["true","1"]:
         return False
     else:
