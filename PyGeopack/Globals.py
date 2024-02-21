7bee164277cc12e667a0d23d3c1a408f1ece0425
matt <mattkjames7@gmail.com>
2024-02-21 00:08:35 +0000

    added function to check for new env variable

--- PyGeopack/Globals.py
+++ PyGeopack/Globals.py
@@ -1,5 +1,13 @@
 import os
 ModulePath = os.path.dirname(__file__)+'/'
 
+def checkWarning():
+    warnVar = os.getenv("PYGEOPACK_NOWARN","")
+    if warnVar.lower() in ["true","1"]:
+        return True
+    else:
+        return False
+showWarnings = checkWarning()
+
 DataPath = None
 DataFile = None
