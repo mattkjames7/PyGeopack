e567bee4154f2870057c3f3913e6020316eddace
Matt James <mattkjames7@gmail.com>
2026-01-15 21:50:53 +0000

    more build fixes

--- tools/container-build.sh
+++ tools/container-build.sh
@@ -13,7 +13,7 @@ docker cp "${SCRIPT_DIR}/create-env.sh" "${CONTAINER}:/home/ubuntu/create-env.sh
 
 docker exec -u ubuntu "${CONTAINER}" bash /home/ubuntu/create-env.sh
 
-docker exec -u ubuntu "${CONTAINER}" bash -c "cd /home/ubuntu/PyGeopack && source /home/ubuntu/env/bin/activate && python3 setup.py bdist_wheel"
+docker exec -u ubuntu "${CONTAINER}" bash -c "cd /home/ubuntu/PyGeopack && source /home/ubuntu/env/bin/activate && python3.14 setup.py bdist_wheel"
 
 docker exec -u ubuntu "${CONTAINER}" bash -c "cd /home/ubuntu/PyGeopack && source /home/ubuntu/env/bin/activate && pip install dist/*.whl"
 
@@ -21,4 +21,4 @@ docker exec -u ubuntu "${CONTAINER}" bash -c "rm -rf /home/ubuntu/PyGeopack"
 
 docker cp "${SCRIPT_DIR}/test_run.py" "${CONTAINER}:/home/ubuntu/test_run.py"
 
-docker exec -u ubuntu "${CONTAINER}" bash -c "source /home/ubuntu/env/bin/activate && python3 /home/ubuntu/test_run.py"
+docker exec -u ubuntu "${CONTAINER}" bash -c "source /home/ubuntu/env/bin/activate && python3.14 /home/ubuntu/test_run.py"
--- tools/create-env.sh
+++ tools/create-env.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-python3 -m venv env
+python3.14 -m venv env
 source env/bin/activate
 pip install --upgrade pip setuptools wheel
 
