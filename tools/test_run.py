1b8e1bd8842c60db936f858a38301238a24dbc00
matt <mattkjames7@gmail.com>
2026-01-15 22:39:13 +0000

    update test output

--- tools/test_run.py
+++ tools/test_run.py
@@ -2,7 +2,7 @@ import PyGeopack as gp
 
 
 field = gp.ModelField(5, 0, 0, 20200101, 12.0, Model='T96')
-field = [x[0] for x in field]
+field = [float(x[0]) for x in field]
 print("Magnetic field at (5,0,0) in GSM at noon on 2020-01-01 (T96): ", field)
 
 trace = gp.TraceField(5, 0, 0, 20200101, 12.0, Model='T96')
